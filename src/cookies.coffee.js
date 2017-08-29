467ab1d06c53770b156ecd7d890d1393261e948e
fy1128 <l.fuying@yahoo.com>
2017-08-29 14:58:42 +0800

    header comments of 'Netscape HTTP Cookie File' added.

--- src/cookies.coffee.js
+++ src/cookies.coffee.js
@@ -9,7 +9,7 @@
       var monthFromNow, output, prefix, segment, _i, _len, _ref;
       monthFromNow = parseInt((new Date()).getTime() / 1000 + 2592000, 10);
       prefix = "." + ($domain.val()) + "\tTRUE\t/\tFALSE\t" + monthFromNow + "\t";
-      output = "";
+      output = "# Netscape HTTP Cookie File\n# http://curl.haxx.se/rfc/cookie_spec.html\n# This is a generated file!  Do not edit.\n\n";
       _ref = $input.val().split(";");
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         segment = _ref[_i];
